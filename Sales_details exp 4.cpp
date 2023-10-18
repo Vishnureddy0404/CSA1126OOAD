1918c11203b04ad9669d9f072acf7c017bd40475
Vishnuvardhanreddy bandi <124245030+vishnureddy1729@users.noreply.github.com>
2023-10-18 20:52:00 +0530

    Add files via upload

---
+++ Sales_details exp 4.cpp
@@ -0,0 +1,26 @@
+#include "Sales_details.h"
+
+// Constructors/Destructors
+//  
+
+Sales_details::Sales_details () {
+initAttributes();
+}
+
+Sales_details::~Sales_details () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void Sales_details::initAttributes () {
+}
+
